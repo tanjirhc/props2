2a153ef7b5d8ef2a9c53ef3fa9f49c9c6c9097b1
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-28 20:40:47 +0600

    ts7006 problem

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import Student from "./Student.tsx"
 function App() {
   return(
     <>
-      <Student name="Tanjir Hasan" age="35" isStudent={false}/>
-      <Student name="Rubina Akter" age="26" isStudent={true}/>
+      <Student name="Tanjir Hasan" age={35} isStudent={false}/>
+      <Student name="Rubina Akter" age={26} isStudent={true}/>
+      <Student name="Farjana Mim" age={17} isStudent={true}/>
+      <Student name="Shahana Sokhi" age={34} isStudent={false}/>
     </>
   );
 }
